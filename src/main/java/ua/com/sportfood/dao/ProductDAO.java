2e8f1a6288ba48c876ceef0e56cefc696c170087
Andrii <makrushinoff@gmail.com>
2021-04-17 14:48:20 +0300

    [ProductDAO] Add ProductDAO impl with tests

---
+++ src/main/java/ua/com/sportfood/dao/ProductDAO.java
@@ -0,0 +1,83 @@
+package ua.com.sportfood.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ua.com.sportfood.models.Product;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Repository
+public class ProductDAO implements GeneralDAO<Product> {
+    private JdbcTemplate jdbcTemplate;
+
+    public ProductDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        List<Product> allProducts = jdbcTemplate.query("SELECT * FROM product", (resultSet, i) -> parseResultSetToProduct(resultSet));
+        return allProducts;
+    }
+
+    @Override
+    public void save(Product product) {
+        jdbcTemplate.update("INSERT INTO product VALUES(?, ?, ?, ?, ?, ?)",
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getArticle(),
+                product.getDescription(),
+                product.getAvailableAmount());
+    }
+
+    @Override
+    public void updateById(UUID id, Product product) {
+        jdbcTemplate.update("UPDATE product SET " +
+                        "name = ?," +
+                        "price = ?," +
+                        "article = ?," +
+                        "description = ?," +
+                        "available_amount = ? WHERE id = ?",
+                product.getName(),
+                product.getPrice(),
+                product.getArticle(),
+                product.getDescription(),
+                product.getAvailableAmount(),
+                id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        jdbcTemplate.update("DELETE FROM product WHERE id = ?", id);
+    }
+
+    @Override
+    public Product findById(UUID id) {
+        List<Product> allProducts = findAll();
+        List<Product> filteredList = allProducts.stream()
+                .filter(product -> product.getId().equals(id))
+                .collect(Collectors.toList());
+        if(filteredList.size() > 1) try {
+            throw new SQLException();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return filteredList.get(0);
+    }
+
+    private Product parseResultSetToProduct(ResultSet resultSet) throws SQLException {
+        Product product = new Product();
+        product.setId(resultSet.getObject("id", UUID.class));
+        product.setName(resultSet.getString("name"));
+        product.setPrice(resultSet.getInt("price"));
+        product.setArticle(resultSet.getString("article"));
+        product.setDescription(resultSet.getString("description"));
+        product.setAvailableAmount(resultSet.getInt("available_amount"));
+        return product;
+    }
+}
---
+++ src/test/java/ua/com/sportfood/dao/ProductDAOTest.java
@@ -0,0 +1,252 @@
+package ua.com.sportfood.dao;
+
+import org.assertj.core.internal.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import ua.com.sportfood.models.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJdbcTest
+@ComponentScan(basePackages = "ua.com.sportfood.dao")
+class ProductDAOTest {
+
+    @Autowired
+    private ProductDAO productDAO;
+    private Product product1;
+    private Product product2;
+    private EmbeddedDatabase database;
+
+    @BeforeEach
+    void setUp() {
+        database = new EmbeddedDatabaseBuilder().
+                setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .build();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        database.shutdown();
+    }
+
+    @Test
+    void shouldFindAll() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+
+        product2 = new Product(
+                "prod2",
+                3000,
+                "PP-02",
+                "Wruper-hruper product2",
+                7
+        );
+        product2.setId(UUID.randomUUID());
+
+        productDAO.save(product1);
+        productDAO.save(product2);
+        Product unexpected = new Product();
+        unexpected.setId(UUID.randomUUID());
+
+        List<Product> result = productDAO.findAll();
+
+        assertThat(result.contains(product1)).isTrue();
+        assertThat(result.contains(product2)).isTrue();
+    }
+
+    @Test
+    void shouldNotFindProduct() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+
+        product2 = new Product(
+                "prod2",
+                3000,
+                "PP-02",
+                "Wruper-hruper product2",
+                7
+        );
+        product2.setId(UUID.randomUUID());
+
+        productDAO.save(product1);
+        productDAO.save(product2);
+        Product unexpected = new Product();
+        unexpected.setId(UUID.randomUUID());
+
+        List<Product> result = productDAO.findAll();
+
+        assertThat(result.contains(unexpected)).isFalse();
+    }
+
+    @Test
+    void shouldSave() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+
+
+        productDAO.save(product1);
+        List<Product> result = productDAO.findAll();
+        assertThat(result.size() == 1).isTrue();
+    }
+
+    @Test
+    void shouldUpdateById() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+
+        product2 = new Product(
+                "prod2",
+                3000,
+                "PP-02",
+                "Wruper-hruper product2",
+                7
+        );
+        product2.setId(product1.getId());
+
+        productDAO.save(product1);
+
+        productDAO.updateById(product1.getId(), product2);
+        List<Product> all = productDAO.findAll();
+        assertThat(all.get(0).equals(product2)).isTrue();
+    }
+
+    @Test
+    void shouldNotFindProductAfterUpdate() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+
+        product2 = new Product(
+                "prod2",
+                3000,
+                "PP-02",
+                "Wruper-hruper product2",
+                7
+        );
+        product2.setId(product1.getId());
+
+        productDAO.save(product1);
+
+        productDAO.updateById(product1.getId(), product2);
+
+        List<Product> all = productDAO.findAll();
+        assertThat(all.get(0).equals(product1)).isFalse();
+    }
+
+    @Test
+    void shouldDeleteById() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+        productDAO.save(product1);
+
+        List<Product> resultListAfterSave = productDAO.findAll();
+        int resultAfterSave = resultListAfterSave.size();
+        productDAO.deleteById(product1.getId());
+        List<Product> resultListAfterDelete = productDAO.findAll();
+        int resultAfterDelete = resultListAfterDelete.size();
+
+        assertThat(resultAfterSave - resultAfterDelete == 1).isTrue();
+    }
+
+    @Test
+    void shouldNotDeleteByIdByIrregularId() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+        productDAO.save(product1);
+
+        List<Product> resultListAfterSave = productDAO.findAll();
+        int resultAfterSave = resultListAfterSave.size();
+        productDAO.deleteById(UUID.randomUUID());
+        List<Product> resultListAfterDelete = productDAO.findAll();
+        int resultAfterDelete = resultListAfterDelete.size();
+
+        assertThat(resultAfterSave - resultAfterDelete == 1).isFalse();
+    }
+
+    @Test
+    void shouldFindById() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+        productDAO.save(product1);
+
+
+        Product result = productDAO.findById(product1.getId());
+
+        assertThat(result.equals(product1)).isTrue();
+    }
+
+    @Test
+    void shouldFindNotByWrongId() {
+        product1 = new Product(
+                "prod1",
+                2000,
+                "PP-01",
+                "Super-puper product",
+                5
+        );
+        product1.setId(UUID.randomUUID());
+        productDAO.save(product1);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> productDAO.findById(UUID.randomUUID()));
+    }
+}
